55159facb95e69631a84de10ca8a69f9c5251569
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-13 18:11:55 -0300

    13/05/20 - 18:11

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+
+            Console.WriteLine("Digite um número para ser calculado na tabuada: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Segue as informações da tabuada: \n");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado = i * numero;
+                Console.WriteLine(i + "x" + numero + "=" + resultado);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
